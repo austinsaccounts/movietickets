6259cd4bc6b86e77c5f56a2e9ebf60c99350d9a3
austin cummings and maly phongsavanh <maly@>
2018-08-20 13:43:15 -0700

    run the function receipt and make ticket prototype defined

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
-
 //business logic
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
-  this.addresses = [];
+function Ticket(showtimes, adults, kids, cardHolder) {
+
+  this.showtimes = parseInt(showtimes);
+  this.adults = parseInt(adults);
+  this.kids = parseInt(kids);
+  this.cardHolder = cardHolder;
+  this.childPrices;
+  this.adultPrices;
+}
+Ticket.prototype.prices = function() {
+  if (this.showtimes === 3) {
+    adultPrices = this.adults * 7;
+    childPrices = this.kids * 3;
+  }
+  else {
+    adultPrices = this.adults * 10;
+    childPrices = this.kids * 5;
+  }
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+Ticket.prototype.receipt = function() {
+  console.log(this);
+  return this.cardHolder + " you have purchased " + this.adults + " adult tickets for " + this.adultPrices + " and " + this.kids + " kids tickets for " + this.childPrices + " for the " + this.showtimes + " 0'clock showing";
 }
-
 
 // user interface logic
 $(document).ready(function() {
@@ -20,12 +33,15 @@ $(document).ready(function() {
     console.log(showtimes + " this is showtime")
     var adults = $("input[name=adults]").val();
     console.log(adults);
-    var children = $("input[name=children]").val();
-    console.log(children);
+    var kids = $("input[name=kids]").val();
+    console.log(kids);
     var cardHolder = $("#cardName").val();
     console.log(cardHolder);
-
-
+    var newTicket = new Ticket(showtimes, adults, kids, cardHolder);
+    // newTicket += newTicket.prices();
+    // console.log(newTicket);
+    $(".ticketOutput").append(newTicket.receipt());
+    console.log(newTicket);
 
       });
 
